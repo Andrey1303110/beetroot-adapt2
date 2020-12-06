34acaad263ec06e5a9ab4dfafa6013204a75d6f2
Andrey1303110 <andrey1303110@gmail.com>
2020-12-06 23:59:39 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+function addColorToNavBar() {
+    let nav = document.querySelector("nav");
+    let windowWidth = window.innerWidth;
+    if (windowWidth > 854) {
+        window.addEventListener("scroll", function () {
+            let scrollPos = window.scrollY;
+            console.log(scrollPos);
+            if (scrollPos > 1, scrollPos < 947) {
+                nav.classList.add('scrolledToHome');
+                nav.classList.remove('scrolledToAbout', 'scrolledToServices', 'scrolledToGallery', 'scrolledToSubscribe', 'scrolledToBlog', 'scrolledToContacts');
+            }
+            else if (scrollPos < 1647) {
+                nav.classList.add('scrolledToAbout');
+                nav.classList.remove('scrolledToHome', 'scrolledToServices', 'scrolledToGallery', 'scrolledToSubscribe', 'scrolledToBlog', 'scrolledToContacts');
+            }
+            else if (scrollPos < 2377) {
+                nav.classList.add('scrolledToServices');
+                nav.classList.remove('scrolledToHome', 'scrolledToAbout', 'scrolledToGallery', 'scrolledToSubscribe', 'scrolledToBlog', 'scrolledToContacts');
+            }
+            else if (scrollPos < 3577) {
+                nav.classList.add('scrolledToGallery');
+                nav.classList.remove('scrolledToHome', 'scrolledToAbout', 'scrolledToServices', 'scrolledToSubscribe', 'scrolledToBlog', 'scrolledToContacts');
+            }
+            else if (scrollPos < 4097) {
+                nav.classList.add('scrolledToSubscribe');
+                nav.classList.remove('scrolledToHome', 'scrolledToAbout', 'scrolledToServices', 'scrolledToGallery', 'scrolledToBlog', 'scrolledToContacts');
+            }
+            else if (scrollPos < 4796) {
+                nav.classList.add('scrolledToBlog');
+                nav.classList.remove('scrolledToHome', 'scrolledToAbout', 'scrolledToServices', 'scrolledToGallery', 'scrolledToSubscribe', 'scrolledToContacts');
+            }
+            else if (scrollPos > 4796) {
+                nav.classList.add('scrolledToContacts');
+                nav.classList.remove('scrolledToHome', 'scrolledToAbout', 'scrolledToServices', 'scrolledToGallery', 'scrolledToSubscribe', 'scrolledToBlog');
+            }
+            else () => nav.classList.add('scrolledStand');
+        });
+    }
+}
+
+addColorToNavBar;
