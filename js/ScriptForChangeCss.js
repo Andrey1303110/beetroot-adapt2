2843328f680e05db6be417a59df5f66f81597cba
Andrey1303110 <andrey1303110@gmail.com>
2020-12-13 20:15:26 +0200

    add image on footer for change theme

--- js/ScriptForChangeCss.js
+++ js/ScriptForChangeCss.js
@@ -1,9 +1,17 @@
 function getNightTheme() {
     let date = new Date();
-    let sysHour = date.getHours()
+    let sysHour = date.getHours();
     if ((sysHour >= 21) || (sysHour <= 6)) {
         document.getElementById('css_path').setAttribute('href', './css/blackstyle.css');
     }
 }
 
 getNightTheme();
+
+function getNightThemeClick() {
+    document.getElementById('css_path').setAttribute('href', './css/blackstyle.css');
+}
+
+function getDayThemeClick() {
+    document.getElementById('css_path').setAttribute('href', './css/style.css');
+}
