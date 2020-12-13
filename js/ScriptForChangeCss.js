8c5987bebb38379ee881991ecf24f5cb4ae32ea4
Andrey1303110 <andrey1303110@gmail.com>
2020-12-13 18:40:52 +0200

    add nightstyle for 1152-1440px

---
+++ js/ScriptForChangeCss.js
@@ -0,0 +1,9 @@
+function getNightTheme() {
+    let date = new Date();
+    let sysHour = date.getHours()
+    if (sysHour <= 21 || sysHour <= 6) {
+        document.getElementById('css_path').setAttribute('href', './css/blackstyle.css');
+    }
+}
+
+getNightTheme();
